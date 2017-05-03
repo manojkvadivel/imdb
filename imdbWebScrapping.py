649b8ca323cf8f742239f69e87a80bf226ad6437
Manoj KV <manojkvcts@gmail.com>
2017-05-03 00:14:40 -0700

    Finalised Code Push

---
+++ imdbWebScrapping.py
@@ -0,0 +1,127 @@
+# coding = utf-8
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import re
+import twitterSentimentAnlaysis
+import csv
+
+__author__ = "Manoj KV"
+
+# ------------------------------------------------------------------#
+"""
+Problem Statement
+IMDB provides a list of celebrities born on the current date. Below is the link:
+http://m.imdb.com/feature/bornondate
+Get the list of these celebrities from this webpage using web scraping (the ones that are displayed i.e top 10). You have to extract the below information:
+     Name of the celebrity
+     Celebrity Image
+     Profession
+     Best Work
+Once you have this list, run a sentiment analysis on twitter for each celebrity and finally the output should be in the below format
+     Name of the celebrity:
+     Celebrity Image:
+     Profession:
+     Best Work:
+    Overall Sentiment on Twitter: Positive, Negative or Neutral
+"""
+# ------------------------------------------------------------------#
+
+"""
+# commented out as its not required\n"
+ class Actor(object):\n"
+     def __init__(self, name = None, image = None, profession = None, bestwork = None):\n"
+         self.name  =  name\n"
+         self.image  =  image\n"
+         self.profession  =  profession\n"
+         self.bestwork  =  bestwork\n")
+"""
+
+
+def get_all_details():
+    """
+    *describe get_all_details function here*
+    :return: *describe return value here*
+    """
+
+    url = "http://m.imdb.com/feature/bornondate/"
+    # print url
+
+    driver = webdriver.Chrome("C:\Program Files (x86)\chromedriver_win32\chromedriver.exe")
+    driver.get(url)  # instruct driver to open the URL in new window
+    driver.maximize_window()  # to See full size of the web page
+
+    html_content = driver.page_source
+    # print html_content
+
+    soup = BeautifulSoup(html_content,"html.parser")
+    # print soup
+    # print(soup.prettify())
+
+    section = soup.find("section","posters list")
+    # print section
+
+    poster = section.findAll("a","poster ")
+    # print poster
+
+    # celebrity_details  =  []  # Create Empty List and later append
+
+    celebrity_json = []  # Create Empty List and later append
+
+    for items in poster:
+        # print items
+
+        name = items.find("span","title").text
+        # print "Name of the celebrity:", name
+
+        image = items.img["src"]
+        # print "Celebrity Image:", image
+
+        details=re.split(",(?=(?:[^']*\'[^']*\')*[^']*$)",items.find("div","detail").text)
+        # print details
+
+        profession = details[0]
+        # profession  =  items.find("div", "detail").text.split(',')[0]
+        # print "Profession:", profession
+
+        bestwork = details[1].strip()
+        # bestwork  =  items.find("div", "detail").text.split(',')[1].strip()
+        # print "Best work:", bestwork.strip()  # remove the leading and trailing white spaces
+        # print '\n'
+
+        # celebrity_details.append(Actor(name, image, profession, bestwork))
+        celebrity_json.append({"name": name,"image": image,"profession": profession,"bestwork": bestwork})
+    driver.close()
+
+    # return celebrity_details
+    return celebrity_json
+
+
+if __name__ == '__main__':
+
+    celebrities = get_all_details()
+    sentiment = twitterSentimentAnlaysis.SentimentAnalysis()
+
+    # for celebrity in celebrities:
+    #     print celebrity.name, celebrity.bestwork, celebrity.profession, celebrity.image
+
+    with open('imdb_celebrity_twitter_sentiment.txt', 'wb') as outfile:
+        outfile.truncate()
+        # writer = csv.writer(outfile, lineterminator='\r\n', quotechar="'")
+        outfile.write("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" + "\n")
+        outfile.write("@@@@@@@@@  Sentiment Analysis for Birthday Celebrities  @@@@@@@@" + "\n")
+        outfile.write("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" + "\n\n")
+
+        for c in range(10):
+            CelebrityName = celebrities[c]["name"]
+            sentiment.celebritysearch(CelebrityName)
+            OverallSentiment = sentiment.sentiment()
+
+            # Writing all the attributes to output file.
+            outfile.write("Name of the celebrity: " + CelebrityName + "\n")
+            outfile.write("Celebrity Image: " + celebrities[c]["image"] + "\n")
+            outfile.write("Profession: " + celebrities[c]["profession"] + "\n")
+            outfile.write("Best Work: " + celebrities[c]["bestwork"] + "\n")
+            outfile.write("Overall Sentiment on Twitter: " + OverallSentiment + "\n\n")
+
+    print("Results are stored in output file 'imdb_celebrity_twitter_sentiment.txt'")
---
+++ twitterSentimentAnlaysis.py
@@ -0,0 +1,107 @@
+import tweepy
+import codecs
+from string import punctuation
+
+# encoding scheme for reading and writing unicode data
+encoding = 'utf-8'
+
+__author__ = "Manoj KV"
+
+
+class SentimentAnalysis:
+
+    def __init__(self):
+        """
+        Initialize Credentials to connect twitter API
+        """
+        credentials = {
+            "twitter": dict(consumer_key="DLw6dt9sOuelQvmdE5kog0fzo",
+                            consumer_secret="HjYdnGb3D3Y43nacx9X8UlDPfxOCbCaIUyYXh5oaGUKADvXZ1m",
+                            access_token="821598672177741826-F72x5lqUSJfqjAUjKJqPmeu11fcAgrf",
+                            access_token_secret="DKO46koLeo12YLO24dm0ip6RpxAhGSaPvqJynLUiNbIN4")
+        }
+
+        oauth_data = credentials["twitter"]  # hardcoded handle for public tweets is sufficient
+        auth = tweepy.OAuthHandler(oauth_data["consumer_key"], oauth_data["consumer_secret"])
+        auth.set_access_token(oauth_data["access_token"], oauth_data["access_token_secret"])
+
+        self.api = tweepy.API(auth)
+
+        self.positive_words = open("positive_words.txt").read().split('\n')
+        # print self.positive_words
+        self.negative_words = open("negative_words.txt").read().split('\n')
+        # print self.negative_words
+
+    def celebritysearch(self, name):
+        """
+        get all the tweets about the celebrities and writing into a text file
+        :rtype: object
+        :param name: 
+        :return: 
+        """
+        output_file = codecs.open("public_tweets.txt", 'w', "utf-8")
+        public_tweets = self.api.search(q=name, lang="en", locale="en", count=1000)
+
+        for tweet in public_tweets:
+            output_file.write(tweet.text + '\n')
+
+        output_file.close()
+
+    def words_count(self, tweets):
+        """
+        get the number of words present in the tweet
+        :param tweets: 
+        :return: 
+        """
+        positive_count = 0
+        negative_count = 0
+
+        for punc in list(punctuation):
+            tweet = tweets.replace(punc, '')
+            words = tweet.lower().split(' ')
+
+        for word in words:
+            if word in self.positive_words:
+                positive_count = positive_count + 1
+                # print word
+            elif word in self.negative_words:
+                negative_count = negative_count + 1
+                # print word
+
+        return positive_count, negative_count
+
+    def sentiment(self):
+        # type: () -> object
+        # type: () -> object
+        """
+        Get all the tweets from "public_tweets.txt" and create a list file
+        and then Print the Sentiment Result
+        :rtype: object
+        :param: 
+        :return: 
+        """
+        tweets_list = codecs.open("public_tweets.txt", 'r', "utf-8").read().split('\n')
+        pos_cnt = 0
+        neg_cnt = 0
+
+        # Get the word count on each tweet stored in list
+        for tweet in tweets_list:
+            if len(tweet):
+                pos, neg = self.words_count(tweet)
+                # print pos, neg
+                pos_cnt = pos_cnt + pos
+                neg_cnt = neg_cnt + neg
+
+        # Logic to decide the sentiment result
+        if pos_cnt > neg_cnt:
+            return "Positive"
+        elif pos_cnt < neg_cnt:
+            return "Negative"
+        else:
+            return "Neutral"
+"""
+if __name__ == '__main__':
+    celebrity = SentimentAnalysis()
+    celebrity.celebritysearch('MS Dhoni')
+    celebrity.sentiment()
+"""
